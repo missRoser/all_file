ca1be9485de22ad91a619c69da0f9084eec65e9a
missRoser <352967821@qq.com>
2016-07-15 18:16:30 +0800

    文件添加

---
+++ argular/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-07-15 16:40:20
+ * @version $Id$
+ */
+
+var app=angular.module("myApp",[]);
+
+app.run(function($rootScope){
+	$rootScope.name="Tom"
+})
